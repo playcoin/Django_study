1af62b5f4d88d80ccb9a17df39d017898439275b
playcoin <seuplaycoin@gmail.com>
2013-03-02 22:26:19 +0800

    mysite test project

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from mysite.views import current_datetime, hours_ahead
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+    (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
